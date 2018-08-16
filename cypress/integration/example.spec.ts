2848dc6d1438a78fde035a19dd57856d8bb6bc5f
Victor Glindås <glinkis@gmail.com>
2018-08-16 12:28:05 +0200

    Simplify test.

--- cypress/integration/example.spec.ts
+++
@@ -1,8 +0,0 @@
-describe("example 1", function() {
-  beforeEach(function() {
-    cy.visit("/dist/");
-  });
-  it("gets example form", function() {
-    cy.get("#example-form");
-  });
-});
---
+++ cypress/integration/test.spec.ts
@@ -0,0 +1,5 @@
+describe("test", function() {
+  it("runs a simple test", function() {
+    expect(true).to.equal(true);
+  });
+});
