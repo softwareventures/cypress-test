a62f894b0055325b10e0c24dff74f0b5eb1859f3
Victor Glindås <glinkis@gmail.com>
2018-08-16 16:10:08 +0200

    Add break before submit button.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,6 +11,7 @@ export class App extends React.Component {
         Last name:
         <br />
         <input type="text" name="lastname" />
+        <br />
         <button type="submit" name="submit">
           Submit
         </button>
