8cf7f0567bbd3038f6be543a3e361cbdaf7baf02
Neale Upstone <neale@nealeupstone.com>
2019-04-10 17:24:55 +0100

    Switch "App" to preact

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
+import {Component, h} from "preact";
 
-export class App extends React.Component {
+export class App extends Component {
   render() {
     return (
       <form id="example-form">
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
 import { App } from "./components/App";
+import {h, render} from "preact";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+// @ts-ignore
+render(<App />, document.getElementById("app"));
